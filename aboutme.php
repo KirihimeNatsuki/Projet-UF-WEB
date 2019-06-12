c2c56699bb90cb0ca8d0a04d3be37229be441ff8
KirihimeNatsuki <enzo.lemarchand@ynov.com>
2019-06-12 16:04:15 +0200

    Ajouts principaux pour le moment.

---
+++ aboutme.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome - Enzo</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+    <link rel="shortcut icon" href="logo.png">
+    <script src="cookies.js"></script>
+  </head>
+  <body>
+    <header>
+      <div id="bandeau-top">
+        <nav>
+          <div class="nav">
+            <ul class="nav navbar-nav">
+						   <li><a href="aboutme.php">A propos</a></li>
+						   <li><a href="blog.php">Blog</a></li>
+						   <li><a href="contact.php">Contact</a></li>
+					  </ul>
+          </div>
+        </nav>
+      </div>
+        <a class="burger"><a href="index.php"><img src="assets/image/tete.png" alt="logo du site (image de moi)"></a>
+        <div class="bandeau-text">
+          <h2>LEMARCHAND ENZO</h2>
+          <h4>Bienvenue dans mon antre</h4>
+        </div>
+    </header>
+    <main>
+      <h1 align="center"><strong>A propos</strong></h1>
+    </main>
+    <footer>
+      <div class="decouvrez">
+
+      </div>
+      <div class="reseaux">
+
+      </div>
+    </footer>
---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome - Enzo</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+    <link rel="shortcut icon" href="logo.png">
+    <script src="cookies.js"></script>
+  </head>
+  <body>
+    <header>
+      <div id="bandeau-top">
+        <nav>
+          <div class="nav">
+            <ul class="nav navbar-nav">
+						   <li><a href="aboutme.php">A propos</a></li>
+						   <li><a href="blog.php">Blog</a></li>
+						   <li><a href="contact.php">Contact</a></li>
+					  </ul>
+          </div>
+        </nav>
+      </div>
+        <a class="burger"><a href="index.php"><img src="assets/image/tete.png" alt="logo du site (image de moi)"></a>
+        <div class="bandeau-text">
+          <h2>LEMARCHAND ENZO</h2>
+          <h4>Bienvenue dans mon antre</h4>
+        </div>
+    </header>
+    <main>
+      <br/>
+      <div class="presentation">
+        <h3 align="center"><strong>Salut, moi c'est Enzo</strong></h3><br/>
+        <p align="center" face="verdana" > Etudiant en informatique (Ingésup) basé à Aix-en-Provence, j'essai de me spécialiser dans le développement logiciel,
+          notament sur les langages C et python, avec un peu de développement web.
+          Je suis aussi en mesure de me déplacer dans les alentours d'Aix en Provence jusqu'à Toulon et d'intégrer facilement une équipe voir en gérer une
+          (peu de formations sur ce sujet mais j'ai des qualités pour ce rôle non négligables).<br/>
+          Je suis régulièrement disponible tous les jours de 9h à 19h pour d'éventuels contact.</p>
+      </div>
+      <br/><br/>
+      <div class="explain">
+        <h3 align="center"><strong>Pourquoi ce site web ?</strong></h3><br/>
+        <p align="center" face="verdana"> Ce site présente ma propre personne, c'est à dire mon parcours, mes expériences, mes compétences, mon CV...<br/>
+          L'idée de départ était de créer un Portfolio en ligne pour un projet de fin d'année d'étude, or j'ai préféré réaliser ce site présentant d'autres fonctionnalités qu'un simple Portfolio
+          afin d'englobaliser tous mes projets et mes expériences professionnelles dans mon propre site<br/>
+          Vous pouvez toujours venir regarder mes projets réalisés et mes stages/expériences récentes dans la partie Blog du site qui propose chaque projet et expérience sous forme d'un topic.
+      </div>
+      <br/><br/>
+      <div class="crea">
+        <h3 align="center"><strong>Création et développement</strong></h3><br/>
+        <p align="center" face="verdana"> La plupart de mes créations sont présentes dans mes répertoires GitHub ou dans mon Portfolio qui est représenté dans la partie A propos que vous pouvez visiter à partir du menu ou ci-dessous:<br/>
+        <a class="btn btn-outline-info" href="aboutme.php" >A propos</a></p>
+      </div>
+      </main>
+      <footer>
+        <div class="decouvrez">
+
+        </div>
+        <div class="reseaux">
+          
+        </div>
+      </footer>
+  </html>
