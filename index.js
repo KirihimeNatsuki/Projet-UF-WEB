5edf3036724ac55c5d4fe6e2fb9228f961d13207
KirihimeNatsuki <enzo.lemarchand@ynov.com>
2019-06-12 14:55:44 +0200

    on corrige le tout début

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Lemarchand Enzo - Portfolio</title>
     <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
     <link rel="shortcut icon" href="logo.png">
     <script src="cookies.js"></script>
@@ -11,19 +12,19 @@
   <body>
     <header>
       <div id="bandeau-top">
-          <a class="burger"><a href="index.php"><img src="assets/image/logo.png" alt="logo du site"></a>
+          <a class="burger"><a href="index.php"><img src="assets/image/tete.png" alt="logo du site"></a>
           <div class="bandeau-text">
-            <h1>UNTITLED.FR</h1>
-            <h4>La référence du jeu vidéo</h4>
+            <h1>LEMARCHAND ENZO</h1>
+            <h3>Bienvenue !</h3>
           </div>
       </div>
       <nav>
         <a href="#"><img id="hide-nav" src="assets/image/menu.svg" alt="" height="75px" width="75px"></a>
         <div class="nav">
-          <a href="index.php">FORUM</a>
-          <a href="loginpage.php">CONNEXION</a>
-          <a href="registerpage.php">INSCRIPTION</a>
-          <a href="membre.php">MON COMPTE</a>
+          <a href="index.js">Accueil</a>
+          <a href="aboutme.php">A propos</a>
+          <a href="blog.php">Blog</a>
+          <a href="contact.php">Contact</a>
         </div>
       </nav>
     </header>
