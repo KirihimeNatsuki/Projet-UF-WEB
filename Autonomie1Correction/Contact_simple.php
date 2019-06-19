1340307d313e86d17f5012ba2ac43b60e7714ba5
KirihimeNatsuki <enzo.lemarchand@ynov.com>
2019-06-19 12:02:14 +0200

    Commencement de la page de contact avec ajouts des dossiers css et images (début des essais).

---
+++ Autonomie1Correction/Contact_simple.php
@@ -0,0 +1,9 @@
+<form name='contact' method='POST' action="#">
+  <textarea name="message" rows="2" cols="80">
+  <input type="submit" name="envoie" placeholder="Envoyer">
+</from>
+
+<?php
+
+
+ ?>
--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Welcome - Enzo</title>
-    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/contact.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
     <link rel="shortcut icon" href="logo.png">
@@ -28,11 +28,23 @@
           <h4>Bienvenue dans mon antre</h4>
         </div>
     </header>
+    <br><br>
     <main>
-      <h1 align="center"><strong>Contactez moi ici !</strong></h1>
-      <div class="contact">
-        <textarea>
-        </textarea>
+      <div class="form-group">
+        <h1 align="center"><strong>Contactez moi ici !</strong></h1>
+        <form action="#" method="POST" name="contact">
+          <label for="nom">Nom</label>
+          <input id="nom" type="text" name="Nom" class="form-control">
+          <label for="prenom">Prénom</label>
+          <input id="prenom" type="text" name="Prenom" class="form-control">
+          <label for="email">Email</label>
+          <input id="email" type="email" name="Email" class="form-control">
+          <label for="message">Message</label>
+          <textarea id="message" name="Message" class="form-control"></textarea>
+          <label for="file">Fichier</label>
+          <input type="file" class="form-control-file" id="file"><br>
+          <input class="btn btn-primary" type="submit" value="Submit"/>
+        </form>
       </div>
     </main>
     <footer>
